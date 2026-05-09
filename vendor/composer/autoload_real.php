095705a7aed9e287a2c8e9e75fdd09297d69e412
deepakindiit <deepakindiit@gmail.com>
2026-05-09 15:36:38 +0530

    added vendor changes

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit4d164970ff114078a26a25640ac334c2
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit4d164970ff114078a26a25640ac334c2', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit4d164970ff114078a26a25640ac334c2', 'loadClassLoader'));
